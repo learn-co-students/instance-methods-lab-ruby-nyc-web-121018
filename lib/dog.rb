e46fede8141a38e0629e84e436a5e2db12e17fcb
Diane <dkorongy@gmail.com>
2018-12-15 11:49:50 -0500

    Update dog.rb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,9 @@
 class Dog
+  def bark
+    puts "Woof!"
+  end
 
-
-def bark
-  puts "Woof!"
+  def sit
+    puts "The Dog is sitting"
+  end
 end
-
-def sit
-  puts " The Dog is sitting."
-end
-
-end
-
